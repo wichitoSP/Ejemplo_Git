689374abef2e4dd01d9bf83ff8df6e42f64f17a0
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-03-19 12:10:32 -0600

    Se ha realizado otro cambio a nuestro proyecto

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        int a;
+        int b;
+
+        int resultado;
+
         System.out.println("Hola esto es una prueba para subir un proyecto en GIT");
 
         System.out.println("Se ha realizado otro cambio a nuestro Repositorio remoto");
+
+        resultado = 1 + 2;
+
+        System.out.println("El resultado es : "+ resultado);
 
 
 
